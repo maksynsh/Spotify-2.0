6543102971299a378632a9a24a2fcde99a68f611
Maksym <max.sharinov@gmail.com>
2022-10-13 13:42:58 +0300

    Header adaptiveness

--- components/Layout/components/Content/index.tsx
+++ components/Layout/components/Content/index.tsx
@@ -13,22 +13,22 @@ export const Content: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className='flex flex-col flex-grow text-white relative'>
-      <header className='flex justify-between h-16 p-2 px-6 absolute left-0 right-0 top-0'>
+      <header className='flex items-center justify-between h-16 p-2 px-6 absolute left-0 right-0 top-0'>
         <Button color='secondary' onClick={() => signOut()}>
           Log out
         </Button>
-        <div className='flex items-center gap-2 bg-black rounded-full p-0.5 pr-2 cursor-pointer ease-in duration-75 opacity-90 hover:opacity-80'>
+        <div className='flex items-center gap-2 bg-black rounded-full h-11 w-11 md:w-auto p-1 md:p-0.5 cursor-pointer ease-in duration-75 opacity-90 hover:opacity-80'>
           {session?.user?.image ? (
             <img
-              className='rounded-full w-10 h-10'
+              className='rounded-full object-cover h-full md:w-10 md:h-10'
               src={session?.user?.image ?? '/user.svg'}
               alt='userImage'
             />
           ) : (
-            <UserIcon className='rounded-full w-10 h-10 text-white border-2 border-white p-1.5' />
+            <UserIcon className='rounded-full object-cover h-full md:w-10 md:h-10 text-white border-2 border-white p-1.5' />
           )}
-          <h4 className='font-semibold mb-0.5'>{session?.user?.name ?? 'user'}</h4>
-          <ChevronDownIcon className='h-5 w-5' />
+          <h4 className='font-semibold mb-0.5 hidden md:block'>{session?.user?.name ?? 'user'}</h4>
+          <ChevronDownIcon className='h-5 w-5 pr-2 hidden md:block' />
         </div>
       </header>
 
