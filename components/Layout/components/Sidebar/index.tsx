2bb11642759e3e776742945a858b92d769b49863
Maksym <max.sharinov@gmail.com>
2022-10-15 01:33:42 +0300

    Sidebar toggle

--- components/Layout/components/Sidebar/index.tsx
+++ components/Layout/components/Sidebar/index.tsx
@@ -8,15 +8,31 @@ import { MenuItem } from './components/MenuItem'
 import { menu } from 'config/menu'
 import { useResizable, useSpotify } from 'hooks'
 import { playlistListState } from 'atoms/playlist'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 
 export const Sidebar = () => {
   const spotifyApi = useSpotify()
   const { asPath: currentPath } = useRouter()
   const { data: session } = useSession()
   const { onDragStart, onDrag } = useResizable({ elementId: 'resizable', min: 200, max: 356 })
-
+  const [hidden, setHidden] = useState(false)
   const [playlists, setPlaylists] =
     useRecoilState<SpotifyApi.PlaylistObjectSimplified[]>(playlistListState)
+
+  const toggleSidebar = () => {
+    let resizable = document.getElementById('resizable')
+    if (!resizable) {
+      return
+    }
+
+    if (!hidden) {
+      setHidden(true)
+      resizable.style.width = '0px'
+      return
+    }
+    setHidden(false)
+    resizable.style.width = '250px'
+  }
 
   useEffect(() => {
     if (spotifyApi.getAccessToken() && playlists.length < 1) {
@@ -35,7 +51,7 @@ export const Sidebar = () => {
     <div
       id='resizable'
       className='flex w-full md:w-64 sticky z-50 bottom-0 md:sticky md:h-screen md:top-0 pt-2.5 md:pt-0
-      bg-gradient-to-b from-transparent via-black/90 to-black md:bg-black'
+      bg-gradient-to-b from-transparent via-black/90 to-black md:bg-black ease-in duration-150'
     >
       <nav className='shadow-lg w-full font-medium text-xs md:text-sm flex flex-col content-between md:pb-3 md:h-screen md:overflow-y-auto scrollbar'>
         <section className={`flex flex-col`}>
@@ -87,6 +103,14 @@ export const Sidebar = () => {
         onDragStart={onDragStart}
         onDrag={onDrag}
       />
+      <div
+        className={`w-6 h-6 hover:brightness-90 box-content ease-in duration-150 bg-green text-white rounded-full border-4 
+      border-black cursor-pointer absolute top-4 -right-3 z-20 flex justify-center items-center 
+        ${hidden && 'rotate-180 -right-6 w-5 h-5 border-0 top-[20px]'}`}
+        onClick={toggleSidebar}
+      >
+        {<ChevronLeftIcon width={20} height={20} />}
+      </div>
     </div>
   )
 }
