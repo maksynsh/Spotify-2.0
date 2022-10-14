b764736eeaf9829bb3fa55c206da0626be09fd04
Maksym <max.sharinov@gmail.com>
2022-10-14 20:59:06 +0300

    Improved Playlist page responsiveness

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -35,8 +35,8 @@ const Playlist: NextPage = ({}) => {
   return (
     <Layout isLoading={isLoading}>
       <GradientSection>
-        <div className='flex align-center gap-4 mx-2 md:mx-8 py-2 md:my-6 h-fit'>
-          <div className='w-40 h-40 md:w-56 md:h-56 shadow-2xl shadow-dark relative'>
+        <div className='flex flex-col sm:flex-row -mt-11 sm:mt-0 align-center gap-4 mx-2 md:mx-8 py-2 md:my-6 h-fit'>
+          <div className='w-36 h-36 sm:w-40 sm:h-40 md:w-56 md:h-56 shadow-2xl shadow-dark relative mx-auto sm:mx-0'>
             <Image
               src={playlist?.images[0]?.url || ''}
               loader={() => playlist?.images[0]?.url || ''}
@@ -45,11 +45,11 @@ const Playlist: NextPage = ({}) => {
             />
           </div>
           <div className='flex flex-col'>
-            <p className='uppercase'>
+            <p className='uppercase hidden sm:block'>
               {playlist?.public && 'public'} {playlist?.type}
             </p>
             <h2
-              className={`h-[5.5rem] md:h-36 xl:h-32 overflow-hidden text-ellipsis font-bold text-2xl md:text-3xl xl:text-6xl playlist-header
+              className={`whitespace-nowrap h-auto sm:h-28 md:h-36 overflow-hidden text-ellipsis sm:whitespace-normal font-bold text-2xl md:text-3xl xl:text-6xl playlist-header
               ${(playlist?.name?.length ?? 1) > 19 && 'text-xl md:text-1xl xl:text-3xl'}
               ${(playlist?.name?.length ?? 1) > 35 && 'text-lg md:text-xl xl:text-2xl'}`}
             >
