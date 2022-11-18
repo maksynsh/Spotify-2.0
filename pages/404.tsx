4eb74bdeb5621efb47e7dd3f5a210b64b988231e
Maksym <max.sharinov@gmail.com>
2022-11-18 20:29:13 +0200

    404 page improvements

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,21 @@
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid'
+import { SpotifyLogo } from 'assets'
 import Link from 'next/link'
 
 export default function PageNotFound() {
   return (
     <div className='h-screen w-screen flex flex-col items-center justify-center gap-2 text-white'>
-      <h1>
-        <b>404</b> - Page Not Found
+      <h1 className='flex flex-col items-center'>
+        <b className='text-4xl flex items-center' aria-label='404'>
+          4<SpotifyLogo className='w-8 h-8' />4
+        </b>
+        <p>Page Not Found</p>
       </h1>
-      <hr className='bg-white w-10' />
+      <hr className='bg-white w-24 mb-4' />
       <Link href='/'>
-        <a className='btn-base'>Go back home</a>
+        <a className='btn-base btn-secondary'>
+          <ArrowLeftOnRectangleIcon className='w-6 h-6' /> Go back home
+        </a>
       </Link>
     </div>
   )
