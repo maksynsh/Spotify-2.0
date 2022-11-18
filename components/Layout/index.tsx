9edbe643bd033f1152f418ae6c554adec7d2d531
Maksym <max.sharinov@gmail.com>
2022-11-18 11:10:37 +0200

    Fixed height bug on mobile browser (now using js)

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import { Player } from 'components'
+import { useDimensions } from 'hooks'
 import { Content } from './components/Content'
 import { Sidebar } from './components/Sidebar'
 
@@ -9,8 +10,10 @@ interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ isLoading = false, gradientOpacity, children }) => {
+  const { height } = useDimensions()
+
   return (
-    <div className='h-screen bg-dark'>
+    <div className='bg-dark' style={{ height: height + 'px' }}>
       <main className='flex flex-col-reverse md:flex-row'>
         <Sidebar />
         <Content gradientOpacity={gradientOpacity}>{!isLoading && children}</Content>
